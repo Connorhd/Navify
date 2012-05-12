1c76e9e74a166e815b56701698512662fc758544
Connor Dunn <connorhd@gmail.com>
2012-05-12 13:41:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Spotify Advanced Nav
+
+An attempt at a better way to manage your current playlist on Spotify
+
+## Todo list
+
+In no particular order.
+
+* Show playlists artists and albums in the top section
+* Filter by playlist, artist, album from the top section
+* Search as you type filter
+* List of your playlists can't be directly accessed by apps (wtf), allow drag+drop of playlist onto app to make it aware and filter by it
+* Multiple playlist/artist/album selection
+* Playback follows cursor
+* Resizable everything (track list columns, top section, top section columns)
+* Sortable columns
+* App icon
---
+++ js/core.js
@@ -0,0 +1,49 @@
+// Initialize the Spotify objects
+var sp = getSpotifyApi(1),
+	models = sp.require("sp://import/scripts/api/models"),
+	views = sp.require("sp://import/scripts/api/views"),
+	ui = sp.require("sp://import/scripts/ui");
+	player = models.player,
+	library = models.library,
+	application = models.application
+
+// Handle items 'dropped' on your icon
+application.observe(models.EVENT.LINKSCHANGED, handleLinks);
+
+function handleLinks() {
+	var links = models.application.links;
+	// Deal with new playlist
+}
+
+$(function(){
+	console.log('Loaded.');
+	
+	// Run on application load
+	handleLinks();
+	
+	var tempPlaylist = new models.Playlist();
+	
+	// Load in users library
+	models.library.tracks.forEach(function (track) {
+		tempPlaylist.add(track);
+	});
+	var FIELD = {
+		ALBUM:      1 << 0,
+		ARTIST:     1 << 1,
+		DOWNLOAD:   1 << 2,
+		DURATION:   1 << 3,
+		IMAGE:      1 << 4,
+		NAME:       1 << 5,
+		NUMBER:     1 << 6,
+		POPULARITY: 1 << 7,
+		PURCHASE:   1 << 8,
+		SHARE:      1 << 9,
+		STAR:       1 << 10,
+		TRACK:      1 << 11,
+		USER:       1 << 12
+	};
+	var playlistList = new views.List(tempPlaylist, function (track) { return new views.Track(track, FIELD.ALBUM | FIELD.NAME | FIELD.ARTIST | FIELD.STAR | FIELD.DURATION);});
+	$("#tracks").empty();
+	$("#tracks").append(playlistList.node);
+						
+});
